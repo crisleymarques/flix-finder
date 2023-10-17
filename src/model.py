6ceb2793f9f40ab8e0891e431e0c669ee4dbafb7
Crisley Venâncio Marques <crisley.marques@ccc.ufcg.edu.br>
2023-10-16 22:11:18 -0300

    chore: improve prompt sendo to GPT API

--- src/model.py
+++ src/model.py
@@ -17,4 +17,15 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 
 
 def get_prompt(movie_description):
-    return f"Me diga um filme de {movie_description.genre} da década de {movie_description.decade} similar à {movie_description.movie_reference}"
+    prompt = f"""
+    Sua tarefa é gerar uma recomendação de um filme ou série de TV para um usuário de uma 
+    aplicação.
+    
+    Recomende um filme do gênero: {movie_description.genre}, 
+    com data de lançamento entre {movie_description.decade} e {movie_description.decade + 9}, 
+    que possua características similares à {movie_description.movie_reference}.
+
+    Sua resposta deve conter o nome do filme ou série de TV, o ano de lançamento e uma breve 
+    descrição sobre ele contendo no máximo 200 caracteres.
+    """
+    return prompt
