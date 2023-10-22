b389bbb778c0de2bb2e9c248b52880b09cf4ee4e
LuizCarlosXavier <62178110+LuizCarlosXavier@users.noreply.github.com>
2023-10-22 20:57:49 -0300

    Melhorando o Prompt

--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
-from model import *
-from data_models import *
+from data_models import MovieDescription
+from model import get_prompt, get_completion
 
 flixfinder = FastAPI()
 
 @flixfinder.get("/")
 def home():
-  return "Welcome to FlixFinder!"
+    return "Welcome to FlixFinder!"
 
 @flixfinder.post("/chat")
 def send_prompt(movie_description: MovieDescription):
-  prompt = get_prompt(movie_description)
-  print(prompt, flush=True)
-
-  return get_completion(prompt)
+    prompt = get_prompt(movie_description)
+    print(prompt, flush=True)
+    return get_completion(prompt)
--- src/model.py
+++ src/model.py
@@ -11,10 +11,9 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
+        temperature=0,
     )
     return response.choices[0].message["content"]
-
 
 def get_prompt(movie_description):
     prompt = f"""
@@ -27,5 +26,38 @@ def get_prompt(movie_description):
 
     Sua resposta deve conter o nome do filme ou série de TV, o ano de lançamento e uma breve 
     descrição sobre ele contendo no máximo 200 caracteres.
+
+    ---
+
+    Explicação da Recomendação:
+
+    {movie_description.movie_reference} é um filme {movie_description.genre} lançado em {movie_description.decade}.
+    Acredito que este filme seja uma excelente recomendação para você, pois compartilha características similares com {movie_description.movie_reference}.
+    Ele é conhecido por {descrição_do_filme_aqui}.
+
+    ---
+
+    Por que eu recomendei este filme?
+
+    Esta recomendação foi feita com base em análises de similaridade entre os atributos de {movie_description.movie_reference} e outros filmes no mesmo gênero e década de lançamento. Acredito que você irá apreciar as semelhanças entre essas obras.
+
+    ---
+
+    Curiosidade sobre {movie_description.movie_reference}:
+
+    Aqui está uma curiosidade interessante sobre o filme {movie_description.movie_reference}:
+    {curiosidade_sobre_o_filme_aqui}.
+
+    ---
+
+    Outra recomendação:
+
+    Além disso, se você gostou de {movie_description.movie_reference}, recomendo também o filme {outro_filme_similar} lançado em {ano_do_outro_filme}.
+    Ele possui uma atmosfera semelhante e tenho certeza de que você vai apreciar.
+
+    ---
+
+    Aproveite a recomendação e divirta-se assistindo!
+
     """
     return prompt
